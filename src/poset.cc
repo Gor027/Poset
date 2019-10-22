6538a6e06a05af17425a56bbaf623e4434b4fa37
talos <darksoldierpl@outlook.com>
2019-10-22 17:56:49 +0200

    made a rough skeleton of the project (v2)

--- src/poset.cc
+++ src/poset.cc
@@ -1,1 +1,37 @@
 #include "poset.h"
+
+unsigned long jnp1::poset_new(void) {
+    return 0;
+}
+
+void jnp1::poset_delete(unsigned long id) {
+
+}
+
+size_t jnp1::poset_size(unsigned long id) {
+    return 0;
+}
+
+bool jnp1::poset_insert(unsigned long id, char const* value) {
+    return false;
+}
+
+bool jnp1::poset_remove(unsigned long id, char const* value) {
+    return false;
+}
+
+bool jnp1::poset_add(unsigned long id, char const* value1, char const* value2) {
+    return false;
+}
+
+bool jnp1::poset_del(unsigned long id, char const* value1, char const* value2) {
+    return false;
+}
+
+bool jnp1::poset_test(unsigned long id, char const* value1, char const* value2) {
+    return false;
+}
+
+void jnp1::poset_clear(unsigned long id) {
+
+}
--- src/poset.h
+++ src/poset.h
@@ -1,4 +1,29 @@
 #ifndef POSET_H
 #define POSET_H
 
+#ifdef __cplusplus
+#include <cstddef>
+#else
+#include <stddef.h>
+#endif
+#include <stdbool.h>
+
+#ifdef __cplusplus
+namespace jnp1 {
+    extern "C" {
+#endif
+        unsigned long poset_new(void);
+        void poset_delete(unsigned long id);
+        size_t poset_size(unsigned long id);
+        bool poset_insert(unsigned long id, char const *value);
+        bool poset_remove(unsigned long id, char const *value);
+        bool poset_add(unsigned long id, char const *value1, char const *value2);
+        bool poset_del(unsigned long id, char const *value1, char const *value2);
+        bool poset_test(unsigned long id, char const *value1, char const *value2);
+        void poset_clear(unsigned long id);
+#ifdef __cplusplus
+    }
+}
+#endif
+
 #endif // POSET_H
