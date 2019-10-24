35c8d6db4556fcc4140ecdb4004ee7afa0d4c4ef
talos <darksoldierpl@outlook.com>
2019-10-24 19:27:54 +0200

    added possible data structures (have to discuss it) (v5)

--- src/poset.cc
+++ src/poset.cc
@@ -7,7 +7,6 @@
 // We shall start with the interface types, namely ids and values; since char const* cannot be stored in the usual
 // way, we shall store proxies for them (in this case, STL strings).
 using poset_id_t = unsigned long;
-using value_t = char const*;
 using persistent_value_t = std::string;
 
 // Now, onto the overall structure of the project.
@@ -28,37 +27,23 @@ using persistent_value_t = std::string;
 // problem of taking representatives from thin air, but we can again utilise the counter system. Therefore, have:
 
 using value_repr_t = uint64_t;
+using names_assoc_t = std::unordered_map<persistent_value_t, value_repr_t>;
+
+value_repr_t value_repr_gen = 0;
+names_assoc_t names_assoc = {};
+
 using poset_t = std::unordered_map<value_repr_t, std::unordered_set<value_repr_t>>;
+using posets_t = std::unordered_map<poset_id_t, poset_t>;
 
-enum { _repr_counter = 0, _names_to_repr = 1 };
-using names_store_t = std::tuple<value_repr_t, std::unordered_map<persistent_value_t, value_repr_t>>;
-
-enum { _id_counter = 0, _id_to_poset = 1 };
-using posets_store_t = std::tuple<poset_id_t, std::unordered_map<poset_id_t, poset_t>>;
-
-enum { _names_store = 0, _posets_store = 1 };
-using posets_t = std::tuple<names_store_t, posets_store_t>;
-
-// Some notes about implementation proper: in the task statement, we are to ensure that name is stored only once for
-// each poset, here we store only once globally. Now, this approach seems better, but perhaps we could ascertain that
-// on the forum. Also, if you wonder what are enums doing here, it's a trick for "more semantic" extraction of data
-// from tuples, as opposed to passing magic numbers to std::get<...>. (As an alternative, we could use structured
-// bindings, when we extract everything from the tuple).
-
-// The "global state" of our program; we use static in accordance to what I was talking about earlier about not
-// polluting linker's namespaces. Now, I'm pretty sure that's how you do it, but I'd have to check up on that some more.
-// Also, functions extraneous to the interface should also be declared static, for the same reason.
-static posets_t posets = {
-    { 0, {} }, // Names store initialization
-    { 0, {} }  // Posets store initialization
-};
+poset_id_t poset_id_gen = 0;
+posets_t posets = {};
 
 unsigned long jnp1::poset_new(void) {
     return 0;
 }
 
 void jnp1::poset_delete(unsigned long id) {
-
+    
 }
 
 size_t jnp1::poset_size(unsigned long id) {
